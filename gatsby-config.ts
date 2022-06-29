4fc6889cb96a7991dca57f2b121420f706deacad
burkle489 <taylerburke48@gmail.com>
2022-06-29 20:34:51 +0100

    config update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,36 +1,41 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `fluid-pokedex`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [ {
-    resolve: 'gatsby-plugin-sass',
-    options: {
-      additionalData: `@import "./src/styles/styles";`,
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `fluid-pokedex`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  },
-  {
-    resolve: 'gatsby-plugin-react-svg',
-    options: {
-      rule: {
-        include: /svgAssets/
-      }
-    }
-  },
-]
-};
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-sass',
+            options: {
+                additionalData: `@import "./src/styles/styles";`,
+            },
+        },
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/svgAssets/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-plugin-react-svg',
+            options: {
+                rule: {
+                    include: /svgAssets/,
+                },
+            },
+        },
+    ],
+}
 
-export default config;
+export default config
